7e5b82d598c29c37b47789635a11c5a42460dd53
Samantha Simmons <sammysimmons274@gmail.com>
2020-02-07 16:45:01 +0000

    inital commit

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 $(document).ready(function() {
-    $("#button_effects1").click(function() {
-        $('#par1').toggle(1000);
+    $("#button_effects1").mouseenter(function() {
+        $('#button_effects1').fadeTo(1000, 0.5);
     });
-    $("#button_effects2").click(function() {
-        $('#par2').toggle(1000);
+    $("#button_effects1").mouseleave(function() {
+        $('#button_effects1').fadeTo(1000, 1);
     });
-    $("#button_effects3").click(function() {
-        $('#par3').toggle(1000);
+
+    $("#button_effects2").mouseenter(function() {
+        $('#button_effects2').fadeTo(1000, 0.5);
     });
-    $("#button_effects4").click(function() {
-        $('#par4').slideToggle(1000);
+    $("#button_effects2").mouseleave(function() {
+        $('#button_effects2').fadeTo(1000, 1);
     });
-    $("#button_effects5").click(function() {
-        $('#par5').slideToggle(1000);
+
+    $("#button_effects3").mouseenter(function() {
+        $('#button_effects3').fadeTo(1000, 0.5);
     });
-    $("#button_effects6").click(function() {
-        $('#par6').slideToggle(1000);
+    $("#button_effects3").mouseleave(function() {
+        $('#button_effects3').fadeTo(1000, 1);
     });
+
+    $("#button_effects4").mouseenter(function() {
+        $('#button_effects4').fadeTo(1000, 0.5);
+    });
+    $("#button_effects4").mouseleave(function() {
+        $('#button_effects4').fadeTo(1000, 1);
+    });
+
+    $("#button_effects5").mouseenter(function() {
+        $('#button_effects5').fadeTo(1000, 0.5);
+    });
+    $("#button_effects5").mouseleave(function() {
+        $('#button_effects5').fadeTo(1000, 1);
+    });
+
+    $("#button_effects6").mouseenter(function() {
+        $('#button_effects6').fadeTo(1000, 0.5);
+    });
+    $("#button_effects6").mouseleave(function() {
+        $('#button_effects6').fadeTo(1000, 1);
+    });
+
 });
